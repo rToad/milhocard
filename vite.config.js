7a808f660e97ed918cf0a5fa3496d95a7a1d190e
rToad <rodrigoec.t@gmail.com>
2026-05-13 21:54:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
+jsimport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/milhocard/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
   }
 })
